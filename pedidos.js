5a5b514ae154d41e50893e611b48c5d627e59366
dsanlazaro <50714841+dsanlazaro@users.noreply.github.com>
2019-06-05 17:05:24 +0200

    Update pedidos.html
    Arreglo del JS de pizza personalizada en el carro

--- pedidos.js
+++ pedidos.js
@@ -1,5 +1,5 @@
 function actualizarPreciosCarrito() {
-    totalP = personalizada1P + ibericaP + barbacoaP + mediterraneaP + yorkP +
+    totalP = personalizadaTotal + ibericaP + barbacoaP + mediterraneaP + yorkP +
         carbonaraP + hawaianaP + quesosP + vegetalP + estacionesP + cocacolaP + fnaranjaP + flimonP;
     totalP = totalP.toFixed(2);
     document.getElementById("precioTotal").innerHTML = ('Precio total: <strong>' + totalP + ' €</strong>');
